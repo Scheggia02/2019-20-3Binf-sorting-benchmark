313e9a7e6960b55ad900d3a58df78ac9a045d87e
daniele leo <danieleleo16@gmail.com>
2020-02-08 09:31:00 +0100

    Add Flask Web Server

--- benchmark_book.py
+++ benchmark_book.py
@@ -1,11 +1,13 @@
 from book import Book
 from selection_sort_book import selection_sort
 
-
-book1 = Book("Calzoniere", "Petrarca")
+book1 = Book("Canzoniere", "Petrarca")
 book2 = Book("Divina Commedia", "Dante")
 book3 = Book("Decamerone", "Boccaccio")
 
 book_list = [book1, book2, book3]
 
 selection_sort(book_list)
+
+for i in range(len(book_list)):
+    print(book_list[i].author)
